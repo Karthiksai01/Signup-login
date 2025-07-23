ae8a5e889079b1a9683f1728d3f4a17567adf0c3
Karthiksai01 <karthiksaibayya24@gmail.com>
2025-07-23 23:36:01 +0530

    Updated Home file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import './Home.css';
+
 function Home() {
   return (
-    <div>
+    <div className="home-container">
       <h1>Hello Karthik!</h1>
       <a href="/login">Login</a> | <a href="/signup">Signup</a>
     </div>
   );
 }
+
 export default Home;
